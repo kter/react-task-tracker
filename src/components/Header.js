bb263e05b710cb1539beeb720d8295f972ebd773
Takahashi Tomohiko <takahashi@tomohiko.io>
2021-04-29 15:03:32 +0900

    styleの2種類の当て方

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 const Header = ({ title }) => {
     return (
         <header>
-            <h1>{title}</h1>
+            <h1 style={{ color: 'red', backgroundColor: 'black' }}>{title}</h1>
+            <h1 style={headingStyle}>{title}</h1>
         </header>
     )
 }
@@ -22,5 +23,9 @@ Header.defaultProps = {
 
 Header.propTypes = {
     title: PropTypes.string.isRequired,
+}
+
+const headingStyle = {
+    color: 'red', backgroundColor: 'black'
 }
 export default Header
