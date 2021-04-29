48e040aa1ae9b68cc0a0d90e51f3a361f91f75be
Takahashi Tomohiko <takahashi@tomohiko.io>
2021-04-29 14:54:40 +0900

    propsの受け渡し

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React from 'react'
+// import React from 'react'
 import Header from './components/Header'
 
-// function App() {
-//   const name = 'Brad'
-//   return (
-//     <div className="container">
-//       <Header />
-//     </div>
-//   );
-// }
-
-class App extends React.Component {
-  render() {
-    return <h1>Hello from a class</h1>
-  }
+function App() {
+  const name = 'Brad'
+  return (
+    <div className="container">
+      <Header title='Hello'/>
+    </div>
+  );
 }
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello from a class</h1>
+//   }
+// }
 export default App;
