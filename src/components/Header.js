8ae67de91bc8b23d61630a1371135df3f28243fd
Takahashi Tomohiko <takahashi@tomohiko.io>
2021-04-29 15:07:47 +0900

    index.cssを使う

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types'
 const Header = ({ title }) => {
     return (
         <header>
+            {/*
             <h1 style={{ color: 'red', backgroundColor: 'black' }}>{title}</h1>
             <h1 style={headingStyle}>{title}</h1>
+            */}
+            <h1>{title}</h1>
         </header>
     )
 }
